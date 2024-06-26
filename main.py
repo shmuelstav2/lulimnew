c741b3d6cde658104982cbb31c681bf9c3aee192
shmuelstav2 <shmhad123@gmail.com>
2024-06-26 23:12:40 +0300

    skila write to the db

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ import pandas as pd
 import openpyxl
 import pandas as pd
 from datetime import datetime, timedelta, date
+from sqlalchemy import create_engine
+import urllib
 import pandas as pd
 import os
+import pandas as pd
+import numpy as np
 #import schedule
 import pandas as pd
 #import xlsxwriter
@@ -42,6 +46,18 @@ excel_prod = config.get('excel_prod', '')
 excel_test = config.get('excel_test', '')
 environment = config.get('environment', '')
 days_ago = 7
+conn_str = (
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=lulimdbserver.database.windows.net;'
+    'DATABASE=lulimdb;'
+    'UID=shmuelstav;'
+    'PWD=5tgbgt5@'
+)
+
+# URL encoding for connection string
+params = urllib.parse.quote_plus(conn_str)
+engine = create_engine(f'mssql+pyodbc:///?odbc_connect={params}')
+
 
 # Print or use the values as needed
 print("Excel Prod:", excel_prod)
@@ -52,6 +68,7 @@ farms = '\\fandy farms'
 reports = '\\current active flocks\\'
 sheet_name_tmuta = 'tmuta'
 sheet_name_sivuk = 'shivuk'
+sheet_name_skila = 'סיכום שקילות'
 excel_file_name_finish = 'current flock '
 excel_end = '.xlsx'''
 excel_middle_name = '\\current flock\\'
@@ -122,6 +139,113 @@ def find_tmuta_iomit(farm, day,df):
     df = df[df['growth day'] == day]
     column_sum = df['mixed daily mortality'].sum()
     return column_sum
+def write_to_mongo_and_delete (df,db_name,collection_name):
+    # MongoDB connection settings
+    mongo_uri = "mongodb://localhost:27017/"
+    database_name = db_name
+    collection_name = collection_name
+
+    # Convert DataFrame to a list of dictionaries
+    data_list = df.to_dict(orient='records')
+
+    # Convert datetime.date objects to datetime.datetime objects
+
+    for record in data_list:
+        for key, value in record.items():
+            if isinstance(value, date):
+                record[key] = value.strftime('%Y-%m-%d')
+    # Connect to MongoDB
+    client = MongoClient(mongo_uri)
+
+    # Access the lulim database (it will be created if it doesn't exist)
+    db = client[database_name]
+
+    # Access the tmuta_end collection
+    collection = db[collection_name]
+
+    # Delete existing data in the collection
+    collection.delete_many({})
+
+    # Insert new data into the collection
+    collection.insert_many(data_list)
+
+    # Close the MongoDB connection
+    client.close()
+def udate_skila():
+    farms_names = subfolder_names(excel_prod + farms)
+    tmuta_results = pd.DataFrame()
+    for farm in farms_names:
+        # check if excel file has changed
+        path = excel_prod + farms + '\\' + farm + excel_middle_name + excel_file_name_finish + farm + excel_end
+        data = read_excel(path, sheet_name_skila)
+        if not data.empty and farm !='sigler' :
+            data = data.replace('', np.nan)
+
+            # Drop rows where all elements are NaN
+            df_cleaned = data.dropna(how='all')
+
+            # Drop columns where all elements are NaN
+            df_cleaned = df_cleaned.dropna(axis=1, how='all')
+            columns_to_keep = [0, 1, 6, 7]
+            df_cleaned = df_cleaned.iloc[:, columns_to_keep]
+            df_cleaned = df_cleaned.dropna()
+
+            new_columns = df_cleaned.iloc[0]  # Get the first row
+
+            # Step 2: Drop the first row from the DataFrame
+            df_cleaned = df_cleaned[1:]  # Drop the first row
+
+            # Step 3: Set new column names
+            df_cleaned.columns = new_columns
+
+            # Optional: Reset the index if needed
+            df_cleaned = df_cleaned.reset_index(drop=True)
+            df = pd.DataFrame()
+
+
+            df['grotwh_day'] = pd.to_numeric(df_cleaned['יום'])
+            df['avg_mixed'] = pd.to_numeric(df_cleaned['ממוצע מעורב'])
+            df['avg_mixed_percent'] = pd.to_numeric(df_cleaned['אחוז תקן מעורב'])
+            df['mivne'] = pd.to_numeric(df_cleaned['מבנה'])
+            df['midgar'] = 1
+            df['farm_name'] = str(farm)
+
+            existing_data = pd.read_sql('SELECT * FROM skila_svuit', con=engine)
+
+            # Normalize data for comparison
+            df['grotwh_day'] = pd.to_numeric(df['grotwh_day'])
+            df['avg_mixed'] = pd.to_numeric(df['avg_mixed'])
+            df['avg_mixed_percent'] = pd.to_numeric(df['avg_mixed_percent'])
+            df['mivne'] = pd.to_numeric(df['mivne'])
+            df['midgar'] = pd.to_numeric(df['midgar'])
+            df['farm_name'] = df['farm_name'].astype(str)
+
+            existing_data['grotwh_day'] = pd.to_numeric(existing_data['grotwh_day'])
+            existing_data['avg_mixed'] = pd.to_numeric(existing_data['avg_mixed'])
+            existing_data['avg_mixed_percent'] = pd.to_numeric(existing_data['avg_mixed_percent'])
+            existing_data['mivne'] = pd.to_numeric(existing_data['mivne'])
+            existing_data['midgar'] = pd.to_numeric(existing_data['midgar'])
+            existing_data['farm_name'] = existing_data['farm_name'].astype(str)
+
+            # Use composite key for comparison
+            composite_key = ['grotwh_day', 'mivne', 'midgar', 'farm_name']
+            existing_data.set_index(composite_key, inplace=True)
+            df.set_index(composite_key, inplace=True)
+
+            # Identify new rows
+            new_rows = df[~df.index.isin(existing_data.index)].reset_index()
+
+            # Insert new rows into the SQL table
+            if not new_rows.empty:
+                new_rows.to_sql('skila_svuit', con=engine, if_exists='append', index=False)
+                print("Inserted new rows successfully.")
+            else:
+                print("No new rows to insert-"+farm)
+    df_view = pd.read_sql("SELECT * FROM dbo.skila_svuit_highest_grotwh_day;", con=engine)
+    write_to_mongo_and_delete(df_view,'lulim_new','skila')
+    print('בכרבר')
+
+
 
 
 def update_data():
@@ -188,38 +312,7 @@ def update_data():
 
             print("Data insertion completed.")
 
-def write_to_mongo_and_delete (df,db_name,collection_name):
-    # MongoDB connection settings
-    mongo_uri = "mongodb://localhost:27017/"
-    database_name = db_name
-    collection_name = collection_name
 
-    # Convert DataFrame to a list of dictionaries
-    data_list = df.to_dict(orient='records')
-
-    # Convert datetime.date objects to datetime.datetime objects
-
-    for record in data_list:
-        for key, value in record.items():
-            if isinstance(value, date):
-                record[key] = value.strftime('%Y-%m-%d')
-    # Connect to MongoDB
-    client = MongoClient(mongo_uri)
-
-    # Access the lulim database (it will be created if it doesn't exist)
-    db = client[database_name]
-
-    # Access the tmuta_end collection
-    collection = db[collection_name]
-
-    # Delete existing data in the collection
-    collection.delete_many({})
-
-    # Insert new data into the collection
-    collection.insert_many(data_list)
-
-    # Close the MongoDB connection
-    client.close()
 def update_results():
 
     # Connect to MongoDB
@@ -294,18 +387,20 @@ def update_results():
     df = pd.DataFrame(list(cursor))
 
     # Group by farm name and find the latest date for each farm
+    try:
+     invalid_date_rows = df[df['marketing date'] == '#VALUE!']
+     # Display the rows with invalid dates
+     print(invalid_date_rows)
+    except KeyError as e:
+        print(f"KeyError: {e}")
 
-    invalid_date_rows = df[df['date'] == '#VALUE!']
+    latest_dates = df.groupby('farm name')['marketing date'].max().reset_index()
 
-    # Display the rows with invalid dates
-    print(invalid_date_rows)
-    latest_dates = df.groupby('farm name')['date'].max().reset_index()
+    filtered_df = pd.merge(df, latest_dates[['farm name', 'marketing date']], on=['farm name', 'marketing date'])
 
-    filtered_df = pd.merge(df, latest_dates[['farm name', 'marketing date']], on=['farm name', 'date'])
+    filtered_df_date = df.groupby('farm name')['marketing date'].max().reset_index()
 
-    filtered_df_date = df.groupby('farm name')['growth day'].max().reset_index()
-
-    filtered_df_date = pd.merge(filtered_df_date, latest_dates[['farm name', 'date']], on=['farm name'])
+    filtered_df_date = pd.merge(filtered_df_date, latest_dates[['farm name', 'marketing date']], on=['farm name'])
 
     filtered_df_date['begin_date'] = pd.to_datetime(filtered_df['date']) - pd.to_timedelta(filtered_df['growth day'],
                                                                                            unit='d')
@@ -347,8 +442,6 @@ def update_results():
     agg_sivuk_small = agg_sivuk_results[
         ['farm name', 'marketing date', 'house', 'receipt', 'destination', 'marketed quantity', 'averrage weight ',
          'marketed age']]
-
-
     write_to_mongo_and_delete(agg_sivuk_small, 'lulim_new', 'sivuk_end')
 
 
@@ -358,7 +451,8 @@ def update_results():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    update_data()
-    update_results()
+    udate_skila()
+    #update_data()
+    #update_results()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
