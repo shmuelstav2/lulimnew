0156d0d1412c8c479cc23365ccba0ae011ee6d2f
shmuelstav2 <shmhad123@gmail.com>
2024-08-02 14:59:26 +0300

    add db to view tmuta14

--- main.py
+++ main.py
@@ -410,9 +410,11 @@ def udate_tmuta():
 
                         # Fetch data and write to MongoDB
                     df_view = pd.read_sql("SELECT * FROM dbo.skila_svuit_highest_grotwh_day;", con=engine)
+                    df_view2 = pd.read_sql("SELECT * FROM dbo.tmuta14;", con=engine)
 
                     # Assuming write_to_mongo_and_delete is a defined function
                     write_to_mongo_and_delete(df_view, 'lulim_new', 'tmuta')
+                    write_to_mongo_and_delete(df_view, 'lulim_new', 'tmuta14')
 
 
 
